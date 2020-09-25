9b8da8ef0a18cec7c9183f0055a05060ccd03771
ari 77 <muhammaddzulfiqar512@gmail.com>
2020-09-25 22:21:21 +0800

    project pertama

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$('.page-scroll').on('click', function(e) {
+
+ var tujuan = $(this).attr('href');
+
+ var elemenTujuan = $(tujuan);
+
+ $('html , body').animate({
+  scrollTop: elemenTujuan.offset().top - 50
+ }, 1000, 'swing');
+
+ e.preventDefault();
+});
+
+//about
+$(window).on('load', function() {
+	$('.pKiri').addClass('pMuncul');
+	$('.pKanan').addClass('pMuncul');
+});
+
+// parallax
+$(window).scroll(function() {
+	var wScroll = $(this).scrollTop();
+
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+	});
+});
